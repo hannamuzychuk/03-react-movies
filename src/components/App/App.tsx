a97e8b25960c155c42adb83ffb90966f45342086
Hanna Muzychuk <hannamuzychuk@gmail.com>
2026-01-20 16:05:22 +0100

    add components

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,21 @@
-import { useState } from 'react'
-import SearchBar from '../SearchBar/SearchBar'
-import styles from "./App.module.css"
+import { useState } from 'react';
+import toast from 'react-hot-toast';
+import SearchBar from '../SearchBar/SearchBar';
+import MovieGrid from '../MovieGrid/MovieGrid';
+import Loader from '../Loader/Loader';
+import ErrorMessage from '../ErrorMessage/ErrorMessage';
+import MovieModal from '../MovieModal/MovieModal';
+import { fetchMovies } from '../../services/movieServise';
+import type { Movie } from '../../types/movie';
+import css from "./App.module.css";
+
+
+export default function App() {
+    const [movies, setMovies] = useState<Movie[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [selectedMovie, setSelectedMovie] = useState<| null>(null);
+}
 
 
 
